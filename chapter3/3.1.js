143d18c70c0f0a2097007b14ed37ea7f0fd116ab
heojihun <karais89@gmail.com>
2015-12-16 22:09:52 +0900

    - global - console - console time

---
+++ chapter3/3.1.js
@@ -0,0 +1,4 @@
+// 현재 실행 중인 코드의 파일 경로를나타냄
+console.log('filename:', __filename);
+// 현재 실행 중인 코드의 폴더 경로를나타냄
+console.log('dirname:', __dirname);
---
+++ chapter3/3.3.js
@@ -0,0 +1,10 @@
+// 시간 측정을 시작합니다.
+console.time('alpha');
+
+var output = 1;
+for (var i = 1; i <= 10; i++) {
+	output *= i;
+}
+console.log('Result :', output);
+// 시간 측정 종료
+console.timeEnd('alpha');
