9941da7dd9f176f508599ffcfead8bb4f9daf040
heojihun <karais89@gmail.com>
2015-12-16 22:14:23 +0900

    - console color

---
+++ chapter3/3.4.js
@@ -0,0 +1,19 @@
+// 글자에 색 적용
+// 문자열 '\u001b[숫자m 을 입력하면 이 이후에  위치하는 모든 출력은 ansi 코드로 
+console.log('\u001b[31m', 'Hello World ..!');
+console.log('\u001b[32m', 'Hello World ..!');
+console.log('\u001b[33m', 'Hello World ..!');
+console.log('\u001b[34m', 'Hello World ..!');
+console.log('\u001b[35m', 'Hello World ..!');
+console.log('\u001b[36m', 'Hello World ..!');
+// 색을 밝게 합니다.
+console.log('\u001b[1m'); 
+console.log('\u001b[31m', 'Hello World ..!');
+console.log('\u001b[32m', 'Hello World ..!');
+console.log('\u001b[33m', 'Hello World ..!');
+console.log('\u001b[34m', 'Hello World ..!');
+console.log('\u001b[35m', 'Hello World ..!');
+console.log('\u001b[36m', 'Hello World ..!');
+
+// 초기화
+console.log('\u001b[0m');
