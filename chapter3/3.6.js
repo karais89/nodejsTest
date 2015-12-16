953a1eda4f88319b25c5e47ec12bb0ced404f83b
heojihun <karais89@gmail.com>
2015-12-16 22:26:57 +0900

    - process

---
+++ chapter3/3.6.js
@@ -0,0 +1,7 @@
+console.log('~ process.env:', process.env);				//  컴퓨터 환경과 관련된 정보
+console.log('~ process.version:', process.version);		//  Nodejs 버전
+console.log('~ process.versions:', process.versions);	//  Nodejs와 종속된 프로그램 버전. 
+console.log('~ process.arch:', process.arch);			//  프로세서의 아키텍쳐.
+console.log('~ process.platform:', process.platform);	//  플랫폼.
+console.log('~ process.memoryUsage():', process.memoryUsage()); // 메모리 사용정보.
+console.log('~ process.uptime():', process.uptime());			// 현재 프로그램 실행된 시간.
