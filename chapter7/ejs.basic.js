a19b6f17dd8820225529606b75ebfa026300d7bd
jh <jh@2mbgame.com>
2015-12-17 17:09:20 +0900

    - ejs data send

--- chapter7/ejs.basic.js
+++ chapter7/ejs.basic.js
@@ -9,7 +9,10 @@ http.createServer(function (request, response) {
 	fs.readFile('ejsPage.ejs', 'utf8', function (error, data) {
 		response.writeHead(200, { 'Content-Type' : 'text/html'});
 		// render -> ejs 문자열을 HTML 문자열로 변경합니다.
-		response.end(ejs.render(data));
+		response.end(ejs.render(data, {
+			name : 'RintIanTta',
+			description: 'Hello ejs With Node.js .. !'
+		}));
 	});
 }).listen(52273, function() {
 	console.log('Server Running at http://127.0.0.1:52273');
