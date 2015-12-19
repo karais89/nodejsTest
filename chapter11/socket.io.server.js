4ed9dac7e82b2c72fc6fc7f12ffa76c2cebc5c26
heojihun <karais89@gmail.com>
2015-12-19 13:02:33 +0900

    -소켓 모듈 기본.

--- chapter11/socket.io.server.js
+++ chapter11/socket.io.server.js
@@ -13,3 +13,21 @@ var server = http.createServer(function (request, response) {
 }).listen(52273, function() {
     console.log('Server running at http://127.0.0.1:52273');
 });
+
+// 소켓 서버를 생성 및 실행합니다.
+var io = socketio.listen(server);
+// Option log level is not valid. Please refer to the Readme.
+// $ npm install debug --save 디버그 모듈을 따로 설치해서 디버그?
+// DEBUG=* node yourfile.js ??
+io.set('log level', 2);
+// io.sockets 객체에 connection 이벤트를 연결.
+io.sockets.on('connection', function (socket) {
+    // rint 이벤트 - 사용자 정의 이벤트.
+    socket.on('rint', function (data) {
+        // 클라이언트가 전송한 데이터 출력.
+        console.log('Client Send Data:', data);
+        
+        // 클라이언트에 smart 이벤트 발생 - 사용자정의 이벤트.
+        socket.emit('smart', data);
+    });
+});
