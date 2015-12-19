f091e0f50c1061507a0a18278881d4afff585311
heojihun <karais89@gmail.com>
2015-12-19 23:16:46 +0900

    - socket room complete

--- chapter11/socket.room.js
+++ chapter11/socket.room.js
@@ -21,5 +21,23 @@ server.on('request', function(request, response) {
 
 // 소켓 서버 이벤트 연결.
 io.sockets.on('connection', function(socket) {
+    // join 이벤트. - 클라이언트가 특정한 방에 접속하게 만드는 이벤트.
+    socket.on('join', function(data) {
+        socket.join(data);         // 클라이언트를 방에 접속하게 만듬.
+        // socket.set('room', data); // 클라이언트에게 자신의 방 번호부여. 
+        socket.room = data;
+    });
     
+    // message 이벤트 - 같은 방에 속한 클라이언트에 메시지를 전달하는 이벤트.
+    socket.on('message', function(data) {
+        // get 메서드를 사용해서 클라이언트 방 번호를 추출.
+        // in 메서드로 현재 접속한 클라이언트 중 같은 방을 공유하는 클라이언트 추출.
+        // 추출한 클라이언트에 emit() 메서드를 사용하여 이벤트를 발생시키면 같은 방에 속하는
+        // 클라이언트에 데이터를 전달 할 수 있습니다.
+        // socket.get('room', function(error, room) {
+        //    io.sockets.in(room).emit('message', data); 
+        // });
+        console.log('message', data);
+         io.sockets.in(socket.room).emit('message', data); 
+    });
 });
