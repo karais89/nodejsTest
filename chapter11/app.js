3b0846846950afa75e1e71e959ac044dc494bcb0
heojihun <karais89@gmail.com>
2015-12-19 14:16:52 +0900

    - 클라이언트 정보 저장 템플릿

---
+++ chapter11/app.js
@@ -0,0 +1,24 @@
+// 모듈 추출.
+var fs = require('fs');
+
+// 서버 생성.
+var server = require('http').createServer();
+var io = require('socket.io').listen(server);
+
+// 서버 실행.
+server.listen(52273, function() {
+   console.log('Server Running at http://127.0.0.1:52273');
+});
+
+// 웹 서버 이벤트 연결.
+server.on('request', function(request, response) {
+    fs.readFile('HTMLPage2.html', 'utf8', function(error, data) {
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.end(data);
+    });
+});
+
+// 소켓 서버 이벤트 연결.
+io.sockets.on('connection', function(socket) {
+    
+});
